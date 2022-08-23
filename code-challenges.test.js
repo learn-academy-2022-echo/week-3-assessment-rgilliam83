43568d9a289e393d0f6e13a1e575ddae6ce18bb8
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-08-23 08:15:41 -0700

    changes

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,3 @@
-//________I am having a extremelly hard time getting my test to pass. Please advise________
-
 // // ASSESSMENT 3: Coding Practical Questions with Jest
 
 // // Please read all questions thoroughly
@@ -13,55 +11,49 @@
 // // Run the file with the following command: $ yarn jest
 
 
-// // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
+// --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
-// // a) Create a test with expect statements for each of the variables provided.
+// a) Create a test with expect statements for each of the variables provided.
 
-// // const fibLength1 = 6
-// // // Expected output: [1, 1, 2, 3, 5, 8]
+const fibLength1 = 6
+// Expected output: [1, 1, 2, 3, 5, 8]
 
-// // const fibLength2 = 10
-// // // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+const fibLength2 = 10
+// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
-// // the call back funtion of the describe contains the it method. the it method takes a string that describes what the test is doing and a call.
-// describe("fibTest", () => {
-//     it("takes in a number greater than 2 and returns an array that length containing the numbers of the Fibonacci sequence", () => {
-//         // within the call back function of the it method, there is an expect statement
-//         const fibLength1 = 6
-//         // Expected output: [1, 1, 2, 3, 5, 8]
+// the call back funtion of the describe contains the it method. the it method takes a string that describes what the test is doing and a call.
+describe("fib", () => {
+    it("takes in a number greater than 2 and returns an array that length containing the numbers of the Fibonacci sequence", () => {
+        
+        expect(fib(fibLength1)).toEqual([1,1,2,3,5,8]);
+        expect(fib(fibLength2)).toEqual([1,1,2,3,5,8,13,21,34,55])
+    })
+})
 
-//         const fibLength2 = 10
-//         // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+describe("fib", () => {
+    it("takes a number", () => {
+        expect(fib(fibLength1)).toEqual([1,1,2,3,5,8])
+        expect(fib(fibLength2)).toEqual([1,1,2,3,5,8,13,21,34,55])
+    })
+})
 
-//         expect(fibTest(fibLength1)).toEqual([1,1,2,3,5,8]);
-//         expect(fibTest(fibLength2)).toEqual([1,1,2,3,5,8,13,21,34,55]);
-//     });
-// });
 
-// describe("fib", () => {
-//     it("takes a number", () => {
-//         expect(fib(fibLength1)).toEqual([1,1,2,3,5,8])
-//         expect(fib(fibLength2)).toEqual([1,1,2,3,5,8,13,21,34,55])
+// write test
+// run testing suite to see a failing test
+// const fibTest = (fibLength1) => {
+//     return fibLength1.valueOf(() => {
+//         for (let fibTest = [i], i=1; i<=2; i++)
+//         fibTest.push(fibLength1[i] + fibLength2[i-1])
+
 //     })
-// })
-
-
-// // write test
-// // run testing suite to see a failing test
-// // const fibTest = (fibLength1) => {
-// //     return fibLength1.valueOf(() => {
-// //         for (let fibTest = [i], i=1; i<=2; i++)
-// //         fibTest.push(fibLength1[i] + fibLength2[i-1])
-
-// //     })
     
-//     const fib = (array) => {
-//         if( fibLength1 === 6 ) {
-//             return [1,1,2,3,5,8]
-//         } else {
-//             return [1,1,2,3,5,8,13,21,34,55]
-//         } 
-//         }
+    const fib = (number) => {
+        let newArr = [0, 1];
+        for (let i = 2; i <= number; i++){
+          newArr.push(newArr[i - 2] + newArr[i - 1])
+        }
+        return newArr.slice(1)
+        }
     
 
 
@@ -96,8 +88,13 @@ describe("onlyOdd", () => {
     })
 })
 
-const onlyOdd = () => {
-    return fullArr1.filter(n => n%2)
+const onlyOdd = (array) => {
+    let newArr =[]
+    for(i = 0; i < array.length; i++){
+        if(array[i] % 2 !== 0 && typeof array[i] === 'number')
+        newArr.push(array[i])
+    }
+    return newArr.sort((a,b) => a-b)
 }
 // b) Create the function that makes the test pass.
 

6b206d6e47d27188cd3c49657cc6152576136891
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2022-08-26 15:09:32 -0700

    code-challenge complete with psuedo code updated, deubugging completed, interviewquestions complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -22,6 +22,13 @@ const fibLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 // the call back funtion of the describe contains the it method. the it method takes a string that describes what the test is doing and a call.
+
+
+//PSEUDO Code:
+    // create describe function. Call it fib.
+    // create the it function with the description "takes in a number greater than 2 and returns an array that length containing the numbers of the Fibonacci sequence"
+    //state desired outcomes with expect statements contained declared variables
+
 describe("fib", () => {
     it("takes in a number greater than 2 and returns an array that length containing the numbers of the Fibonacci sequence", () => {
         
@@ -30,38 +37,26 @@ describe("fib", () => {
     })
 })
 
-describe("fib", () => {
-    it("takes a number", () => {
-        expect(fib(fibLength1)).toEqual([1,1,2,3,5,8])
-        expect(fib(fibLength2)).toEqual([1,1,2,3,5,8,13,21,34,55])
-    })
-})
+
+//Test failed due to fib fuuction not being defined
+
+// b) Create the function that makes the test pass.
+    //declare function fib
 
 
-// write test
-// run testing suite to see a failing test
-// const fibTest = (fibLength1) => {
-//     return fibLength1.valueOf(() => {
-//         for (let fibTest = [i], i=1; i<=2; i++)
-//         fibTest.push(fibLength1[i] + fibLength2[i-1])
-
-//     })
-    
-    const fib = (number) => {
+const fib = (number) => {
+    //declare new array
         let newArr = [0, 1];
+        // establish for loop that will interate through array
         for (let i = 2; i <= number; i++){
           newArr.push(newArr[i - 2] + newArr[i - 1])
         }
+        // create return for desired output
         return newArr.slice(1)
         }
+
+        //test passed
     
-
-
-
-
-
-// b) Create the function that makes the test pass.
-
 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
@@ -73,20 +68,24 @@ const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
 const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // Expected output: [-823, 7, 23]
 
+
+//PSEUDO Code:
+        //create describe function. Call it onlyOdd.
+        // create the it function with the description "Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest"
+        //state desired outcomes with expect statements contained declared variables
+
 describe("onlyOdd", () => {
     it("Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest", () =>{
-
-        const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
-        // Expected output: [-9, 7, 9, 199]
-        
-        const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
-        // Expected output: [-823, 7, 23]
 
         expect(onlyOdd(fullArr1)).toEqual([-9, 7, 9, 199])
         expect(onlyOdd(fullArr2)).toEqual([-823, 7, 23])
         
     })
 })
+
+//Test failed due to onlyOdd fuuction not being defined
+
+// b) Create the function that makes the test pass.
 
 const onlyOdd = (array) => {
     let newArr =[]
@@ -96,9 +95,6 @@ const onlyOdd = (array) => {
     }
     return newArr.sort((a,b) => a-b)
 }
-// b) Create the function that makes the test pass.
-
-
 
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
@@ -114,5 +110,35 @@ const numbersToAdd2 = [0, 7, -8, 12]
 const numbersToAdd3 = []
 // Expected output: []
 
+//PSEUDO Code:
+        //create describe function. Call it accumulatingSum.
+        // create the it function with the description "create a function that takes in an array and returns an array of the accumulating sum"
+        //state desired outcomes with expect statements contained declared variables
+
+describe("accumulatingSum", () => {
+    it("create a function that takes in an array and returns an array of the accumulating sum", () => {
+        expect(accumulatingSum(numbersToAdd1)).toEqual([2, 6, 51, 60])
+        expect(accumulatingSum(numbersToAdd2)).toEqual([0, 7, -1, 11])
+        expect(accumulatingSum(numbersToAdd3)).toEqual([])
+    })
+})
+
+
+//Test failed due to accumulatingSum fuuction not being defined
 
 // b) Create the function that makes the test pass.
+
+
+const accumulatingSum = (array) => {
+    let new_array = []
+    if (array.length > 0) {
+     for(let i = 1; i < array.length; i++) {
+        new_array[0] = array[0]
+        new_array[i] = new_array[i-1] + array[i]    
+    }
+    
+    
+}
+return new_array
+
+}
